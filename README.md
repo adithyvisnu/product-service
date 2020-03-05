6fc4359ac32b7f5e8d2d45ab3004ada3c144e231
Adithya Visnu <adithyavisnu@gmail.com>
2020-03-05 14:39:51 +0700

    add project layouts

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # product-service
 Testing apps with golang for product services
+
+## Layouts
+Project Layout is copied from https://github.com/golang-standards/project-layout
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	println("Hello Developers! Welcome to Product Service built with Go and love")
+	println(`
+	===============================
+	|        @adithyvisnu         |
+	|       Product Service       |
+	|             luv             |
+	===============================
+	`)
+}
